d2fac7950cbb0e1924b144bf202edbc14e2271c3
Mina Sayed <menaboss13@gmail.com>
2023-12-21 17:47:09 +0200

    Solve CampModule

--- src/app.module.ts
+++ src/app.module.ts
@@ -6,11 +6,11 @@ import { CampginModule } from './campgin/campgin.module';
   imports: [
     TypeOrmModule.forRoot({
       type: 'postgres',
-      host: 'viaduct.proxy.rlwy.net',
-      port: 49949,
+      host: 'localhost',
+      port: 5432,
       username: 'postgres',
-      password: '4fA5g*26EAbAGe3D6-1DACdf2EDd5a2f',
-      database: 'railway',
+      password: 'mina',
+      database: 'postgres',
       entities: [__dirname + '/**/*.entity{.ts,.js}'],
       synchronize: true,
     }),
